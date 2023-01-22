88a5fef735b434a9a1512fc808d82c7b2bbabaec
Andreas Trichopoulos <atrichop@yahoo.com>
2023-01-22 17:14:21 +0200

    Fix csv row writer

--- cht_oil_monitor/sensor_dict_test_csv.py
+++ cht_oil_monitor/sensor_dict_test_csv.py
@@ -10,31 +10,27 @@ import csv
 if not os.path.exists('Rides'):
     os.mkdir('Rides')
 
-starttime = time.time()
 filename = datetime.now().strftime('Rides/Ride_%Y.%m.%d.csv')
 
 
 def flow():
     with open(filename, 'a', newline='') as csvfile:
-        field_names = ['Date', 'Time', 'Cyl 1', 'Cyl 2', 'Oil']
+        field_names = ['Date', 'Time'] + list(amplifiers.keys())
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
 
-        writer.writerow(
-            {'Date': time.strftime("%d:%m:%Y", time.localtime(time.time())),
-             'Time': time.strftime("%H:%M", time.localtime(time.time()))
-             })
+        csv_row = {'Date': time.strftime("%d/%m/%Y", time.localtime(time.time())),
+                   'Time': time.strftime("%H:%M", time.localtime(time.time()))}
 
-        temperatures = []
-        while len(temperatures) < len(amplifiers):
-            for name, value in amplifiers.items():
-                try:
-                    amplifier = value["board"](value["spi"], value["cs"])
-                    temp_c = amplifier.temperature
-                    print(name, ": {} C".format(temp_c))
-                    writer.writerow({"{}".format(temp_c)})
-                except RuntimeError as cyl_rte:
-                    print(name, ":", cyl_rte)
-                time.sleep(2.0)
+        for key, value in amplifiers.items():
+            try:
+                amplifier = value["board"](value["spi"], value["cs"])
+                temp_c = amplifier.temperature
+                print(key, ": {} C".format(temp_c))
+                csv_row[key] = temp_c
+            except RuntimeError as cyl_rte:
+                print(key, ":", cyl_rte)
+            time.sleep(2.0)
+        writer.writerow(csv_row)
 
 
 while True:
