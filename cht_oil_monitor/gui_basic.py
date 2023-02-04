c9c3870939b6207334b16d97e908c96cca1d550a
Andreas Trichopoulos <atrichop@yahoo.com>
2023-02-04 20:51:05 +0200

    Basic gui with engine sketch

--- cht_oil_monitor/gui_basic.py
+++ cht_oil_monitor/gui_basic.py
@@ -1,12 +1,21 @@
 from guizero import App, Drawing
 
-a = App(title="Digital Bug", height=500, width=500)
+a = App(title="Digital Bug", width=800, height=480, bg="black",)
+a.set_full_screen()
+# a.full_screen = True
 
 # create drawing object
 d = Drawing(a, width=500, height=500)
-d.rectangle(50, 50, 300, 300, color="light blue")
-d.rectangle(20, 30, 120, 120, color="grey")
-d.line(50, 180, 50, 160, color="red", width=5)
-
+d.rectangle(150, 125, 300, 140, color=(100,100,100), outline=3, outline_color=(160, 160, 160))
+d.rectangle(150, 150, 300, 400, color=(100,100,100), outline=5, outline_color=(160, 160, 160))
+d.rectangle(180, 410, 270, 420, color=(100,100,100), outline=2, outline_color=(160, 160, 160))
+d.rectangle(60, 250, 150, 180, color=(100,100,100), outline=5, outline_color=(160, 160, 160))
+d.rectangle(60, 350, 150, 280, color=(100,100,100), outline=5, outline_color=(160, 160, 160))
+d.rectangle(300, 200, 390, 270, color=(100,100,100), outline=5, outline_color=(160, 160, 160))
+d.rectangle(300, 300, 390, 370, color=(100,100,100), outline=5, outline_color=(160, 160, 160))
+d.text(305, 220, "165\u2103", color="red", size=24)
+d.text(75, 200, "160\u2103", color="red", size=24)
+# d.text(190, 245, f"90 {chr(176)} C", color="red", size=24)
+d.text(190, 245, "95\u2103", color="red", size=24)
 
 a.display()
