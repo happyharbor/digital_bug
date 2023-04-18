532ab5267a017de3d540cd307b9ff0b11e1f0b0c
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2023-04-18 14:01:02 +0300

    Save measurement to db

--- cht_oil_monitor/sensor_write_to_db.py
+++ cht_oil_monitor/sensor_write_to_db.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 import time
-import uuid
 
 from amplifier_config import amplifiers
 import psycopg2
 
-conn = psycopg2.connect("dbname='bug' user='postgres' host='localhost' password='postgres'")
-
+conn = psycopg2.connect(dbname='bug', user='postgres', host='localhost', password='postgres')
 cur = conn.cursor()
 
 
 def flow():
+    cur.execute(""" INSERT INTO measurement (type) VALUES (%s) RETURNING id""", ('temperature',))
+    measurement_id = cur.fetchone()[0]
     for key, value in amplifiers.items():
         try:
             amplifier = value["board"](value["spi"], value["cs"])
             temp_c = amplifier.temperature
-            # cur.execute("""INSERT INTO measurement (type) VALUES (%s)""", 'temperature')
             cur.execute("""INSERT INTO temperature (measurement_id, source, temperature) VALUES (%s, %s, %s)""",
-                        (str(uuid.uuid4()), key, temp_c))
-            # rows = cur.fetchall()
-            conn.commit()
-            count = cur.rowcount
-            print(count, "Record inserted successfully into measurement table")
+                        (measurement_id, key, temp_c))
 
         except (Exception, psycopg2.Error) as error:
             print("Failed to insert record into measurement table", error)
@@ -29,6 +24,7 @@ def flow():
         except RuntimeError as cyl_rte:
             print(key, ":", cyl_rte)
         time.sleep(2.0)
+    conn.commit()
 
 
 while True:
